16d8f2c721c9511b6e6a8f7d73f724144a11600e
Nikolaev <anikolaev@anikolaev-lws>
2016-11-18 16:22:19 +0200

    Added changes for Policy_Update SDL behaviour when PT 1MB

--- test_scripts/Polices/Policies_Security/001_ATF_P_PolicyTable_GeneralSDL_behavior.lua
+++ test_scripts/Polices/Policies_Security/001_ATF_P_PolicyTable_GeneralSDL_behavior.lua
@@ -1,3 +1,7 @@
+----  Name of requirement that is covered.
+----- [General] Policies enough memory for PTS
+----  Policies Manager must have enough memory to hold a Policy Table Snapshot in memory.
+
 --------------------------------------------------------------------------------------------
 -----------------------------Required Shared Libraries---------------------------------------
 ---------------------------------------------------------------------------------------------
@@ -8,34 +12,37 @@ local commonSteps = require('user_modules/shared_testcases/commonSteps')
 config.defaultProtocolVersion = 2
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
+--[[ Local preparing ]]
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+--[[ Preconditions ]]
 commonFunctions:userPrint(33, "================= Precondition ==================")
 Test = require('connecttest')
 
-function Test:GeneralSDL_Behaviour_PTmoreThan_1mb()
+--[[Test Case]]
+function Test:GeneralSDLbehaviour_PTsize_1mb()
 commonFunctions:userPrint(33, "================= Test Case ======================")
--- local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-       local pathToSnaphot = nil
-       EXPECT_HMICALL ("BasicCommunication.PolicyUpdate")
-      :Do(function(_,data)
-        pathToSnaphot = data.params.file
-        print ("Way for snapshot of BD", pathToSnaphot)     
-        self.hmiConnection:SendResponse(data.id, "BasicCommunication.PolicyUpdate", "SUCCESS", {})
-      end)
-
- --EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {url = "http://policies.telematics.ford.com/api/policies"}}})
-:Do(function(_,data)
-  self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
-  {
-   requestType = "PROPRIETARY",
-   url = "http://policies.telematics.ford.com/api/policies",
-   appID = self.applications ["Test Application"],
-   fileName = "sdl_snapshot.json"
+ local pathToSnaphot = nil
+  EXPECT_HMICALL ("BasicCommunication.PolicyUpdate")
+  :Do(function(_,data)
+    pathToSnaphot = data.params.file
+    self.hmiConnection:SendResponse(data.id, "BasicCommunication.PolicyUpdate", "SUCCESS", {})
+   end)
+    local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+    EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {url = "http://policies.telematics.ford.com/api/policies"}}})
+    :Do(function(_,data)
+     self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
+    {
+     requestType = "PROPRIETARY",
+     url = "http://policies.telematics.ford.com/api/policies",
+     appID = self.applications ["Test Application"],
+     fileName = "sdl_snapshot.json"
   }, 
    pathToSnaphot 
   )
  end)
   EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "PROPRIETARY" })
-   
+ 
  :Do(function(_,data)
    local CorIdSystemRequest = self.mobileSession:SendRPC ("SystemRequest",
    {
@@ -51,8 +58,9 @@ commonFunctions:userPrint(33, "================= Test Case =====================
  end)
   EXPECT_RESPONSE(CorIdSystemRequest, { success = true, resultCode = "SUCCESS"})
  :Do(function(_,data)
-  os.execute("cp /home/anikolaev/OpenSDL_AUTOMATION/test_run/files/ptu.json /tmp/fs/mp/images/ivsu_cache/")
-  self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
+ --Copy of JSON file size near 1_MB in /tmp/fs/mp/images/ivsu_cache/
+ --Example: os.execute("cp /home/anikolaev/OpenSDL_AUTOMATION/test_run/files/ptu.json /tmp/fs/mp/images/ivsu_cache/")
+   self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
  {
    policyfile = "/tmp/fs/mp/images/ivsu_cache/ptu.json"
  })
