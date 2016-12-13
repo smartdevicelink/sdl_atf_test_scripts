49ef4bd1e493111c1533406374b48841156e6d49
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2016-12-13 14:04:08 +0200

    Make script ready

--- test_scripts/Polices/build_options/ATF_HMILvl_on_PTU_affected_in_FULL_or_LIMITED_PROPRIETARY.lua
+++ test_scripts/Polices/build_options/ATF_HMILvl_on_PTU_affected_in_FULL_or_LIMITED_PROPRIETARY.lua
@@ -1,7 +1,5 @@
---UNREADY
--- function AddApplicationToPTJsonFile should be added to the testCasesForPolicyTable.lua
-
 ---------------------------------------------------------------------------------------------
+-- PROPRIETARY flow
 -- Requirement summary:
 -- [PolicyTableUpdate] HMILevel on Policy Update for the apps affected in FULL/LIMITED
 --
@@ -12,65 +10,140 @@
 -- b) device is connected to SDL and is consented by the User
 -- 2. Performed steps
 -- 1) register the app_1 and activate in FULL HMILevel
--- 2) Update PTU.
--- 3) register the app_2 and activate in LIMITED HMILevel
--- 4) Update PTU.
+-- 2) register the app_2 and activate in LIMITED HMILevel
+-- 3) Update PTU for app_1
+-- 4) Update PTU for app_2
 --
 -- Expected result:
 -- 1) appID_1 remains in FULL. After PTU OnHMIStatus does not calls
 -- 2) appID_2 remains in LIMITED. After PTU OnHMIStatus does not calls
--- 3) After each PTU OnPermissionsChange is called
+-- 3) After PTU OnPermissionsChange is sent for each app
 
 ---------------------------------------------------------------------------------------------
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
-local commonSteps = require('user_modules/shared_testcases/commonSteps')
-local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
-local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
-local json = require('json')
+local mobileSession = require("mobile_session")
+local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
+local commonSteps = require("user_modules/shared_testcases/commonSteps")
+local testCasesForBuildingSDLPolicyFlag = require('user_modules/shared_testcases/testCasesForBuildingSDLPolicyFlag')
+
 --[[ Local Variables ]]
-local HMIAppID
+local policy_file_path = commonFunctions:read_parameter_from_smart_device_link_ini("SystemFilesPath")
+local policy_file_name = "PolicyTableUpdate"
+local ptu_file_1 = "files/jsons/Policies/build_options/ptu_18599_1.json"
+local ptu_file_2 = "files/jsons/Policies/build_options/ptu_18599_2.json"
+local sequence = { }
+local r_actual_hmi_levels = { }
+local r_actual_OnPermissionsChange = { }
 
--- Basic PTU file
-local basic_ptu_file = "files/ptu.json"
--- PTU for first app
-local ptu_first_app_registered = "files/ptu1app.json"
--- PTU for Second app
-local ptu_second_app_registered = "files/ptu2app.json"
+--[[ Local Functions ]]
+local function timestamp()
+  local f = io.popen("date +%H:%M:%S.%3N")
+  local o = f:read("*all")
+  f:close()
+  return (o:gsub("\n", ""))
+end
 
--- Prepare parameters for app to save it in json file
-local function PrepareJsonPTU1(name, new_ptufile)
-  local json_app = [[ {
-    "keep_context": false,
-    "steal_focus": false,
-    "priority": "NONE",
-    "default_hmi": "NONE",
-    "groups": [
-    "Base-4", "Location-1"
-    ],
-    "RequestType":[
-    "TRAFFIC_MESSAGE_CHANNEL",
-    "PROPRIETARY",
-    "HTTP",
-    "QUERY_APPS"
-    ]
-  }]]
-  local app = json.decode(json_app)
-  testCasesForPolicyTable:AddApplicationToPTJsonFile(basic_ptu_file, new_ptufile, name, app)
+local function log(event, ...)
+  table.insert(sequence, { ts = timestamp(), e = event, p = {...} })
+end
+
+local function get_app_hmi_id(self, id)
+  return self.applications["App_" .. id]
+end
+
+local function register_default_app(self, id)
+  EXPECT_HMICALL("BasicCommunication.UpdateAppList")
+  :Do(function(_, d)
+      self.hmiConnection:SendResponse(d.id, d.method, "SUCCESS", { })
+      self.applications = { }
+      for _, app in pairs(d.params.applications) do
+        self.applications[app.appName] = app.appID
+      end
+    end)
+  local corId = self["mobileSession" .. id]:SendRPC("RegisterAppInterface", config["application" .. id].registerAppInterfaceParams)
+  self["mobileSession" .. id]:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
+end
+
+local function start_mobile_session(self, id)
+  self["mobileSession" .. id] = mobileSession.MobileSession(self, self.mobileConnection)
+  self["mobileSession" .. id]:StartService(7)
+end
+
+local function activate_app(self, id)
+  local requestId1 = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = get_app_hmi_id(self, id) })
+  EXPECT_HMIRESPONSE(requestId1)
+end
+
+local function register_OnHMIStatus(self, id)
+  self["mobileSession" .. id]:ExpectNotification("OnHMIStatus")
+  :Do(function(_, d)
+      log("SDL->MOB" .. id .. ": OnHMIStatus()", d.payload.hmiLevel)
+      r_actual_hmi_levels[id] = d.payload.hmiLevel
+    end)
+  :Times(AnyNumber())
+  :Pin()
+end
+
+local function register_OnPermissionsChange(self, id)
+  self["mobileSession" .. id]:ExpectNotification("OnPermissionsChange")
+  :Do(function(_, d)
+      log("SDL->MOB" .. id .. ": OnPermissionsChange()", d.payload.requestType)
+      r_actual_OnPermissionsChange[id] = true
+    end)
+  :Times(AnyNumber())
+  :Pin()
+end
+
+local function ptu(self, id, ptu_file)
+  local requestId = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+  EXPECT_HMIRESPONSE(requestId)
+  :Do(function(_, _)
+      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
+        { requestType = "PROPRIETARY", fileName = policy_file_name, appID = get_app_hmi_id(self, id) })
+      self["mobileSession" .. id]:ExpectNotification("OnSystemRequest", { requestType = "PROPRIETARY" })
+      :Do(function(_, _)
+          local corIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", { requestType = "PROPRIETARY", fileName = policy_file_name }, ptu_file)
+          EXPECT_HMICALL("BasicCommunication.SystemRequest")
+          :Do(function(_, data)
+              self.hmiConnection:SendResponse(data.id, "BasicCommunication.SystemRequest", "SUCCESS", { })
+              self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate", { policyfile = policy_file_path .. "/" .. policy_file_name })
+            end)
+          EXPECT_RESPONSE(corIdSystemRequest, { success = true, resultCode = "SUCCESS" })
+          :Do(function(_, _)
+              requestId = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage", {language = "EN-US", messageCodes = { "StatusUpToDate" }})
+              EXPECT_HMIRESPONSE(requestId)
+            end)
+        end)
+    end)
+end
+
+local function check_file_exists(name)
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
 end
 
 --[[ General Precondition before ATF start ]]
-commonSteps:DeleteLogsFileAndPolicyTable()
---ToDo(vvvakulenko): shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
-config.defaultProtocolVersion = 2
+testCasesForBuildingSDLPolicyFlag:CheckPolicyFlagAfterBuild("PROPRIETARY")
+commonFunctions:SDLForceStop()
 
 --[[ General Settings for configuration ]]
-Test = require('connecttest')
-local mobile_session = require('mobile_session')
-require('cardinalities')
-require('user_modules/AppTypes')
+Test = require("connecttest")
+require("user_modules/AppTypes")
+-- config.defaultProtocolVersion = 3
+for i = 1, 2 do
+  config["application" .. i].registerAppInterfaceParams.appName = "App_" .. i
+end
+config.application1.registerAppInterfaceParams.isMediaApplication = false
+config.application1.registerAppInterfaceParams.appHMIType = { "NAVIGATION" }
+config.application2.registerAppInterfaceParams.appHMIType = { "DEFAULT" }
 
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
@@ -78,6 +151,16 @@ commonFunctions:newTestCasesGroup("Preconditions")
 function Test.Precondition_StopSDL()
   StopSDL()
 end
+
+function Test.Precondition_Clean()
+  commonSteps:DeleteLogsFileAndPolicyTable()
+  os.remove(config.pathToSDL .. "/app_info.dat") -- in order to skip resumption
+  os.remove(policy_file_path .. "/sdl_snapshot.json")
+  if not check_file_exists(policy_file_path .. "/sdl_snapshot.json") then
+    print("PTS is removed")
+  end
+end
+
 function Test.Precondition_StartSDL()
   StartSDL(config.pathToSDL, config.ExitOnCrash)
 end
@@ -94,87 +177,100 @@ function Test:Precondition_ConnectMobile()
   self:connectMobile()
 end
 
-function Test:Precondition_StartSession()
-  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
+function Test:StartMobileSession_1()
+  start_mobile_session(self, 1)
 end
 
-function Test.Precondition_PreparePTData()
-  PrepareJsonPTU1(config.application1.registerAppInterfaceParams.appID, ptu_first_app_registered)
-  PrepareJsonPTU1(config.application2.registerAppInterfaceParams.appID, ptu_second_app_registered)
+function Test:StartMobileSession_2()
+  start_mobile_session(self, 2)
+end
+
+function Test:Register_OnHMIStatus()
+  register_OnHMIStatus(self, 1)
+  register_OnHMIStatus(self, 2)
+end
+
+function Test:RegisterApp_1()
+  register_default_app(self, 1)
+end
+
+function Test:RegisterApp_2()
+  register_default_app(self, 2)
+end
+
+function Test:ActivateApp_1()
+  activate_app(self, 1) -- app1 -> FULL
+end
+
+function Test:ActivateApp_2()
+  activate_app(self, 2) -- app2 -> FULL, app1 -> LIMITED
+end
+
+function Test:Register_OnPermissionsChange()
+  register_OnPermissionsChange(self, 1)
+  register_OnPermissionsChange(self, 2)
 end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
-function Test:TestStep_RegisterFirstApp()
-  self.mobileSession:StartService(7)
-  :Do(function (_,_)
-      local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
 
-      EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
-      :Do(function(_,data)
-          HMIAppID = data.params.application.appID
-        end)
-      EXPECT_RESPONSE(correlationId, { success = true })
-      EXPECT_NOTIFICATION("OnPermissionsChange")
-    end)
+function Test:Test_PTU_1()
+  ptu(self, 1, ptu_file_1)
 end
 
-function Test:TestStep_ActivateAppInFull()
-  commonSteps:ActivateAppInSpecificLevel(self,HMIAppID,"FULL")
+function Test:Test_UPDATING()
+  local reqId = self.hmiConnection:SendRequest("SDL.GetStatusUpdate")
+  EXPECT_HMIRESPONSE(reqId, {status = "UPDATING"})
 end
 
-function Test:TestStep_UpdatePolicyAfterAddFirstAp_ExpectOnHMIStatusNotCall()
-  EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
-
-  testCasesForPolicyTable:updatePolicyInDifferentSessions(Test, ptu_first_app_registered,
-    config.application1.registerAppInterfaceParams.appName,
-    self.mobileSession)
-  self.mobileSession:ExpectNotification("OnPermissionsChange")
-
-  self.mobileSession:ExpectNotification("OnHMIStatus"):Times(0)
+function Test:Test_PTU_2()
+  ptu(self, 2, ptu_file_2)
 end
 
-function Test:TestStep_RegisterSecondApp()
-  self.mobileSession1 = mobile_session.MobileSession(self, self.mobileConnection)
-
-  self.mobileSession1:StartService(7)
-  :Do(function (_,_)
-      local correlationId = self.mobileSession1:SendRPC("RegisterAppInterface", config.application2.registerAppInterfaceParams)
-
-      EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
-      :Do(function(_,data)
-          HMIAppID = data.params.application.appID
-        end)
-      self.mobileSession1:ExpectResponse(correlationId, { success = true })
-      self.mobileSession1:ExpectNotification("OnPermissionsChange")
-    end)
+function Test:Test_UP_TO_DATE()
+  local reqId = self.hmiConnection:SendRequest("SDL.GetStatusUpdate")
+  EXPECT_HMIRESPONSE(reqId, {status = "UP_TO_DATE"})
 end
 
-function Test:TestStep_ActivateSecondAppInLimited()
-  commonSteps:ActivateAppInSpecificLevel(self,HMIAppID,"LIMITED")
+function Test.Test_ShowSequence()
+  print("--- Sequence -------------------------------------")
+  for k, v in pairs(sequence) do
+    local s = k .. ": " .. v.ts .. ": " .. v.e
+    for _, val in pairs(v.p) do
+      if val then s = s .. ": " .. val end
+    end
+    print(s)
+  end
+  print("--------------------------------------------------")
 end
 
-function Test:TestStep_UpdatePolicyAfterAddSecondApp_ExpectOnHMIStatusNotCall()
-  EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
+function Test:Test_Validation_OnHMIStatus()
+  local r_expected_hmi_levels = { "LIMITED", "FULL" }
+  for i = 1, 2 do
+    if r_expected_hmi_levels[i] ~= r_actual_hmi_levels[i] then
+      local msg = table.concat({
+          "\nExpected OnHMIStatus() level for app '", i, "' is '", r_expected_hmi_levels[i],
+          "', but actual is '", r_actual_hmi_levels[i], "'"})
+      self:FailTestCase(msg)
+    end
+  end
+end
 
-  testCasesForPolicyTable:updatePolicyInDifferentSessions(Test, ptu_second_app_registered,
-    config.application2.registerAppInterfaceParams.appName,
-    self.mobileSession1)
-  self.mobileSession1:ExpectNotification("OnPermissionsChange")
-  self.mobileSession1:ExpectNotification("OnHMIStatus"):Times(0)
-
+function Test:Test_Validation_OnPermissionsChange()
+  local r_expected_OnPermissionsChange = { true, true }
+  for i = 1, 2 do
+    if r_expected_OnPermissionsChange[i] ~= r_actual_OnPermissionsChange[i] then
+      local msg = table.concat({"\nExpected OnPermissionsChange() notification for app '", i, "' was not sent"})
+      self:FailTestCase(msg)
+    end
+  end
 end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
 
-function Test.Postcondition_RemovePTUfiles()
-  os.remove(ptu_first_app_registered)
-  os.remove(ptu_second_app_registered)
-end
-
-function Test:Postcondition_Force_Stop_SDL()
-  commonFunctions:SDLForceStop(self)
+function Test.Postconditions_StopSDL()
+  StopSDL()
 end
 
 return Test
