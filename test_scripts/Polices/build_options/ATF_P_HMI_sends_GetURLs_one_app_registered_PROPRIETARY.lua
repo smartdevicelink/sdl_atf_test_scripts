3d6549a036d846acdf5a48e46ec8bdbd56012d8f
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2016-12-13 16:50:47 +0200

    Make script Ready

--- test_scripts/Polices/build_options/ATF_P_HMI_sends_GetURLs_one_app_registered_PROPRIETARY.lua
+++ test_scripts/Polices/build_options/ATF_P_HMI_sends_GetURLs_one_app_registered_PROPRIETARY.lua
@@ -1,8 +1,3 @@
--- UNREADY:
--- Please note still not implemented functions:
--- testCasesForPolicyTable:flow_PTU_SUCCEESS_PROPRIETARY() and testCasesForPolicyTable:trigger_PTU_user_press_button_HMI
--- testCasesForPolicyTable:flow_PTU_SUCCEESS_PROPRIETARY() correct when fucntion will be included in testCasesForPolicyTable
-
 ---------------------------------------------------------------------------------------------
 -- Requirements summary:
 -- In case HMI sends GetURLs and at least one app is registered SDL must return only default url and url related to registered app
@@ -20,70 +15,157 @@
 --
 -- Expected result:
 -- PTU is requested. PTS is created.
--- SDL.GetURLs({urls[] = registered_App1, default})
+-- SDL.GetURLs({urls[] = default, registered_App1})
 ---------------------------------------------------------------------------------------------
-
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
-local commonSteps = require('user_modules/shared_testcases/commonSteps')
-local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
-local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
-local testCasesForPolicyTableSnapshot = require('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
+local mobileSession = require("mobile_session")
+local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
+local commonSteps = require("user_modules/shared_testcases/commonSteps")
+local testCasesForBuildingSDLPolicyFlag = require('user_modules/shared_testcases/testCasesForBuildingSDLPolicyFlag')
+
+--[[ Local Variables ]]
+local sequence = { }
+local ptu_file = "files/jsons/Policies/build_options/ptu_14831.json"
+
+--[[ Local Functions ]]
+local function timestamp()
+  local f = io.popen("date +%H:%M:%S.%3N")
+  local o = f:read("*all")
+  f:close()
+  return (o:gsub("\n", ""))
+end
+
+local function log(event, ...)
+  table.insert(sequence, { ts = timestamp(), e = event, p = {...} })
+end
+
+local function show_log()
+  print("--- Sequence -------------------------------------")
+  for k, v in pairs(sequence) do
+    local s = k .. ": " .. v.ts .. ": " .. v.e
+    for _, val in pairs(v.p) do
+      if val then s = s .. ": " .. val end
+    end
+    print(s)
+  end
+  print("--------------------------------------------------")
+end
+
+local function is_table_equal(t1, t2)
+  local ty1 = type(t1)
+  local ty2 = type(t2)
+  if ty1 ~= ty2 then return false end
+  if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
+  for k1, v1 in pairs(t1) do
+    local v2 = t2[k1]
+    if v2 == nil or not is_table_equal(v1, v2) then return false end
+  end
+  for k2, v2 in pairs(t2) do
+    local v1 = t1[k2]
+    if v1 == nil or not is_table_equal(v1, v2) then return false end
+  end
+  return true
+end
+
+local function ptu(self, file)
+  local policy_file_path = commonFunctions:read_parameter_from_smart_device_link_ini("SystemFilesPath")
+  local policy_file_name = "PolicyTableUpdate"
+  local requestId = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+  EXPECT_HMIRESPONSE(requestId)
+  :Do(function(_, _)
+      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest", { requestType = "PROPRIETARY", fileName = policy_file_name })
+      self.mobileSession:ExpectNotification("OnSystemRequest", { requestType = "PROPRIETARY" })
+      :Do(function(_, _)
+          local corIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", { requestType = "PROPRIETARY", fileName = policy_file_name }, file)
+          EXPECT_HMICALL("BasicCommunication.SystemRequest")
+          :Do(function(_, data)
+              self.hmiConnection:SendResponse(data.id, "BasicCommunication.SystemRequest", "SUCCESS", { })
+              self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate", { policyfile = policy_file_path .. "/" .. policy_file_name })
+            end)
+          EXPECT_RESPONSE(corIdSystemRequest, { success = true, resultCode = "SUCCESS" })
+          :Do(function(_, _)
+              requestId = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage", {language = "EN-US", messageCodes = { "StatusUpToDate" }})
+              EXPECT_HMIRESPONSE(requestId)
+            end)
+        end)
+    end)
+end
 
 --[[ General Precondition before ATF start ]]
+testCasesForBuildingSDLPolicyFlag:CheckPolicyFlagAfterBuild("PROPRIETARY")
+commonFunctions:SDLForceStop()
 commonSteps:DeleteLogsFileAndPolicyTable()
 
---TODO(anikolaev): Should be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
-config.defaultProtocolVersion = 2
-
 --[[ General Settings for configuration ]]
-Test = require('connecttest')
-require('cardinalities')
-require('user_modules/AppTypes')
+Test = require("connecttest")
+require("user_modules/AppTypes")
+
+--[[ Specific Notifications ]]
+EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate")
+:Do(function(_, d)
+    log("SDL->HMI: SDL.OnStatusUpdate()", d.params.status)
+  end)
+:Times(AnyNumber())
+:Pin()
 
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
---TODO(anikolaev): Function still not implemented
-function Test.Precondition_PTU_SUCCESS ()
-  testCasesForPolicyTable:flow_PTU_SUCCEESS_PROPRIETARY()
-end
-
---TODO(anikolaev): Function still not implemented
-function Test.Precondition_Request_PTU ()
-  testCasesForPolicyTable:trigger_PTU_user_press_button_HMI()
+function Test:PTU()
+  ptu(self, ptu_file)
 end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
 
-function Test:TestStep_PTU_GetURLs()
-  local endpoints = {}
-  local is_app_esxist = false
+function Test:StartNewMobileSession()
+  self.mobileSession2 = mobileSession.MobileSession(self, self.mobileConnection)
+  self.mobileSession2:StartService(7)
+end
 
-  for i = 1, #testCasesForPolicyTableSnapshot.pts_endpoints do
-    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "0x07") then
-      endpoints[#endpoints + 1] = { url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value, appID = nil}
-    end
-    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "app1") then
-      endpoints[#endpoints + 1] = { url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value, appID = nil}
-      is_app_esxist = true
-    end
-  end
-
-  local RequestId = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-
-  EXPECT_HMIRESPONSE(RequestId,{result = {code = 0, method = "SDL.GetURLS", urls = endpoints}})
-  :Do(function(_,_)
-      if(is_app_esxist == false) then
-        self:FailTestCase("endpoints for application doesn't exist!")
+function Test:RegisterNewApp()
+  EXPECT_HMICALL("BasicCommunication.UpdateAppList")
+  :Do(function(_, d)
+      self.hmiConnection:SendResponse(d.id, d.method, "SUCCESS", { })
+      self.applications = { }
+      for _, app in pairs(d.params.applications) do
+        self.applications[app.appName] = app.appID
       end
     end)
+  local corId = self.mobileSession2:SendRPC("RegisterAppInterface", config.application2.registerAppInterfaceParams)
+  self.mobileSession2:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
+end
+
+function Test:PTU()
+  EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
+  :Do(function(_, _)
+      local requestId = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+      EXPECT_HMIRESPONSE(requestId)
+      :ValidIf(function(_, d)
+          local r_expected = { "http://policies.telematics.ford.com/api/policies", "http://policies.domain2.ford.com/api/policies" }
+          local r_actual = d.result.urls
+          if not is_table_equal(r_expected, r_actual) then
+            local msg = table.concat({
+                "\nExpected:\n", commonFunctions:convertTableToString(r_expected, 1),
+                "\nActual:\n", commonFunctions:convertTableToString(r_actual, 1)})
+            return false, msg
+          end
+          return true
+        end)
+    end)
+end
+
+function Test.Test_ShowSequence()
+  show_log()
 end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
-Test["StopSDL"] = function()
+
+function Test.Postconditions_StopSDL()
   StopSDL()
 end
+
+return Test
