b183c3e067f449c1641b8e7aaca4f405bb350e39
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2016-12-13 09:53:03 +0200

    Make script ready

--- test_scripts/Polices/build_options/ATF_Apply_PTU_and_OnPermissionChange_notifying_apps_PROPRIETARY.lua
+++ test_scripts/Polices/build_options/ATF_Apply_PTU_and_OnPermissionChange_notifying_apps_PROPRIETARY.lua
@@ -1,7 +1,5 @@
---UNREADY
--- function AddApplicationToPTJsonFile should be added to the testCasesForPolicyTable.lua
-
 ---------------------------------------------------------------------------------------------
+-- PROPRIETARY flow
 -- Requirement summary:
 -- [PolicyTableUpdate] Apply PTU changes and OnPermissionChange notifying the apps
 --
@@ -31,133 +29,249 @@
 -- 4. SDL removes 'policyfile' from the directory
 -- 5. SDL->appID_1: onPermissionChange(permisssions)
 -- 6. SDL->HMI: SDL.OnAppPermissionChanged(appID_1, permissions)
-
+---------------------------------------------------------------------------------------------
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
-local commonSteps = require('user_modules/shared_testcases/commonSteps')
-local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
-local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
-local json = require('json')
+local json = require("modules/json")
+local mobileSession = require("mobile_session")
+local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
+local commonSteps = require("user_modules/shared_testcases/commonSteps")
+local testCasesForBuildingSDLPolicyFlag = require('user_modules/shared_testcases/testCasesForBuildingSDLPolicyFlag')
 
 --[[ Local Variables ]]
-local HMIAppID
--- Basic PTU file
-local basic_ptu_file = "files/ptu.json"
--- PTU for registered app
-local ptu_app_registered = "files/ptu_app.json"
+local policy_file_path = commonFunctions:read_parameter_from_smart_device_link_ini("SystemFilesPath")
+local policy_file_name = "PolicyTableUpdate"
+local ptu_file = "files/jsons/Policies/build_options/ptu_18592.json"
+local sequence = { }
+local r_actual = { }
 
--- Prepare parameters for app to save it in json file
-local function PrepareJsonPTU1(name, new_ptufile)
-  local json_app = [[ {
-    "keep_context": false,
-    "steal_focus": false,
-    "priority": "NONE",
-    "default_hmi": "NONE",
-    "groups": [
-    "Base-4", "Emergency-1"
-    ],
-    "RequestType":[
-    "TRAFFIC_MESSAGE_CHANNEL",
-    "PROPRIETARY",
-    "HTTP",
-    "QUERY_APPS"
-    ]
-  }]]
-  local app = json.decode(json_app)
-  testCasesForPolicyTable:AddApplicationToPTJsonFile(basic_ptu_file, new_ptufile, name, app)
+--[[ Local Functions ]]
+local function timestamp()
+  local f = io.popen("date +%H:%M:%S.%3N")
+  local o = f:read("*all")
+  f:close()
+  return (o:gsub("\n", ""))
+end
+
+local function log(event, ...)
+  table.insert(sequence, { ts = timestamp(), e = event, p = {...} })
+end
+
+local function check_file_exists(name)
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+local function json_to_table(file)
+  local f = io.open(file, "r")
+  if f == nil then error("File not found") end
+  local ptString = f:read("*all")
+  f:close()
+  return json.decode(ptString)
+end
+
+local function is_table_equal(t1, t2)
+  local ty1 = type(t1)
+  local ty2 = type(t2)
+  if ty1 ~= ty2 then return false end
+  if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
+  for k1, v1 in pairs(t1) do
+    local v2 = t2[k1]
+    if v2 == nil or not is_table_equal(v1, v2) then return false end
+  end
+  for k2, v2 in pairs(t2) do
+    local v1 = t1[k2]
+    if v1 == nil or not is_table_equal(v1, v2) then return false end
+  end
+  return true
 end
 
 --[[ General Precondition before ATF start ]]
+testCasesForBuildingSDLPolicyFlag:CheckPolicyFlagAfterBuild("EXTERNAL_PROPRIETARY")
+commonFunctions:SDLForceStop()
 commonSteps:DeleteLogsFileAndPolicyTable()
---ToDo(vvvakulenko): shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
-config.defaultProtocolVersion = 2
 
 --[[ General Settings for configuration ]]
-Test = require('connecttest')
-local mobile_session = require('mobile_session')
-require('cardinalities')
-require('user_modules/AppTypes')
+Test = require("connecttest")
+require("user_modules/AppTypes")
+
+--[[ Specific Notifications ]]
+EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate")
+:Do(function(_, d)
+    log("SDL->HMI: SDL.OnStatusUpdate()", d.params.status)
+    if d.params.status == "UP_TO_DATE" then
+      table.insert(r_actual, "SDL.OnStatusUpdate(UP_TO_DATE)")
+    end
+  end)
+:Times(AnyNumber())
+:Pin()
+
+EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
+:Do(function(_, _)
+    log("SDL->HMI: BC.PolicyUpdate()")
+  end)
+:Times(AnyNumber())
+:Pin()
+
+EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
+:Do(function(_, _)
+    log("SDL->HMI: BC.OnAppRegistered()")
+  end)
+:Times(AnyNumber())
+:Pin()
+
+EXPECT_HMINOTIFICATION("SDL.OnAppPermissionChanged")
+:Do(function(_, _)
+    log("SDL->HMI: SDL.OnAppPermissionChanged()")
+    table.insert(r_actual, "SDL.OnAppPermissionChanged")
+  end)
+:Times(AnyNumber())
+:Pin()
 
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
-
-function Test.Precondition_StopSDL()
-  StopSDL()
-end
-
-function Test.Precondition_StartSDL()
-  StartSDL(config.pathToSDL, config.ExitOnCrash)
-end
-
-function Test:Precondition_initHMI()
-  self:initHMI()
-end
-
-function Test:Precondition_initHMI_onReady()
-  self:initHMI_onReady()
-end
-
-function Test:Precondition_ConnectMobile()
-  self:connectMobile()
-end
-
-function Test:Precondition_StartSession()
-  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
-end
-
-function Test.Precondition_PreparePTData()
-  PrepareJsonPTU1(config.application1.registerAppInterfaceParams.appID, ptu_app_registered)
+function Test.CleanData()
+  os.remove(policy_file_path .. "/sdl_snapshot.json")
+  if not check_file_exists(policy_file_path .. "/sdl_snapshot.json") then
+    print("PTS is removed")
+  end
 end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
-function Test:TestStep_RegisterApp()
-  self.mobileSession:StartService(7)
-  :Do(function (_,_)
-      local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
 
-      EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
-      :Do(function(_,data)
-          HMIAppID = data.params.application.appID
+function Test:RegisterEvents()
+  self.mobileSession:ExpectNotification("OnPermissionsChange")
+  :Do(function(_, _)
+      log("SDL->MOB: OnPermissionsChange()")
+      table.insert(r_actual, "OnPermissionsChange")
+    end)
+  :Times(AnyNumber())
+  :Pin()
+end
+
+function Test:PTU()
+  local requestId = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+  log("HMI->SDL: SDL.GetURLS")
+  EXPECT_HMIRESPONSE(requestId)
+  :Do(function(_, _)
+      log("SDL->HMI: SDL.GetURLS")
+      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest", {requestType = "PROPRIETARY", fileName = policy_file_name})
+      log("HMI->SDL: BC.OnSystemRequest")
+      EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "PROPRIETARY"})
+      :Do(function(_, _)
+          log("SDL->MOB: OnSystemRequest")
+          local corIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", {requestType = "PROPRIETARY", fileName = policy_file_name}, ptu_file)
+          log("MOB->SDL: SystemRequest")
+          EXPECT_HMICALL("BasicCommunication.SystemRequest")
+          :Do(function(_, data)
+              log("SDL->HMI: BC.SystemRequest")
+              self.hmiConnection:SendResponse(data.id, "BasicCommunication.SystemRequest", "SUCCESS", {})
+              log("HMI->SDL: BC.SystemRequest")
+              self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate", {policyfile = policy_file_path .. "/" .. policy_file_name})
+              log("HMI->SDL: SDL.OnReceivedPolicyUpdate")
+            end)
+          EXPECT_RESPONSE(corIdSystemRequest, { success = true, resultCode = "SUCCESS"})
+          :Do(function(_, _)
+              log("SDL->MOB: SystemRequest")
+              requestId = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage", {language = "EN-US", messageCodes = {"StatusUpToDate"}})
+              log("HMI->SDL: SDL.GetUserFriendlyMessage")
+              EXPECT_HMIRESPONSE(requestId)
+              log("SDL->HMI: SDL.GetUserFriendlyMessage")
+            end)
         end)
-      EXPECT_RESPONSE(correlationId, { success = true })
-      EXPECT_NOTIFICATION("OnPermissionsChange")
     end)
 end
 
-function Test:TestStep_ActivateAppInFull()
-  commonSteps:ActivateAppInSpecificLevel(self,HMIAppID,"FULL")
+function Test.ShowSequence()
+  print("--- Sequence -------------------------------------")
+  for k, v in pairs(sequence) do
+    local s = k .. ": " .. v.ts .. ": " .. v.e
+    for _, val in pairs(v.p) do
+      if val then s = s .. ": " .. val end
+    end
+    print(s)
+  end
+  print("--------------------------------------------------")
 end
 
-function Test:TestStep_UpdatePolicy_ExpectOnAppPermissionChangedWithAppID()
-  EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
+function Test:ValidateSnapshot()
+  if check_file_exists(policy_file_path .. "/sdl_snapshot.json") then
+    self:FailTestCase("PTS is NOT removed")
+  end
+end
 
-  testCasesForPolicyTable:updatePolicyInDifferentSessions(Test, ptu_app_registered,
-    config.application1.registerAppInterfaceParams.appName,
-    self.mobileSession)
+function Test:StartNewMobileSession()
+  self.mobileSession2 = mobileSession.MobileSession(self, self.mobileConnection)
+  self.mobileSession2:StartService(7)
+end
 
-  EXPECT_NOTIFICATION("OnPermissionsChange")
-  :ValidIf(function (_, data)
-      if data.payload~=nil then
-        return true
-      else
-        print("OnPermissionsChange came without permissions")
-        return false
+function Test:RegisterNewApp()
+  EXPECT_HMICALL("BasicCommunication.UpdateAppList")
+  :Do(function(_, d)
+      self.hmiConnection:SendResponse(d.id, d.method, "SUCCESS", { })
+      self.applications = { }
+      for _, app in pairs(d.params.applications) do
+        self.applications[app.appName] = app.appID
       end
     end)
+  local corId = self.mobileSession2:SendRPC("RegisterAppInterface", config.application2.registerAppInterfaceParams)
+  self.mobileSession2:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
+end
+
+function Test:Validate_OnStatusUpdate()
+  local exp = "SDL.OnStatusUpdate(UP_TO_DATE)"
+  if r_actual[1] ~= exp then
+    self:FailTestCase(table.concat({"\nExpected notification ", exp, " was not sent"}))
+  end
+end
+
+function Test:Validate_OnPermissionsChange()
+  local exp = "OnPermissionsChange"
+  if r_actual[2] ~= exp then
+    self:FailTestCase(table.concat({"\nExpected notification ", exp, " was not sent"}))
+  end
+end
+
+function Test:Validate_OnAppPermissionChanged()
+  local exp = "SDL.OnAppPermissionChanged"
+  if r_actual[3] ~= exp then
+    self:FailTestCase(table.concat({"\nExpected notification ", exp, " was not sent"}))
+  end
+end
+
+function Test:Validate_PTS()
+  local pts = json_to_table(policy_file_path .. "/sdl_snapshot.json") -- actual
+  local ptu = json_to_table(ptu_file) -- expected
+  -- Reconcile expected vs actual
+  ptu.policy_table.module_config.preloaded_pt = false
+  pts.policy_table.app_policies["0000001"].certificate = nil
+
+  -- Compare
+  if not is_table_equal(ptu.policy_table.functional_groupings, pts.policy_table.functional_groupings) then
+    self:FailTestCase("Diffs in 'functional_groupings' section\nExpected:\n" .. commonFunctions:convertTableToString(ptu.policy_table.functional_groupings, 1) .. "\nActual:\n" .. commonFunctions:convertTableToString(pts.policy_table.functional_groupings, 1))
+  end
+  if not is_table_equal(ptu.policy_table.module_config, pts.policy_table.module_config) then
+    self:FailTestCase("Diffs in 'module_config' section\nExpected:\n" .. commonFunctions:convertTableToString(ptu.policy_table.module_config, 1) .. "\nActual:\n" .. commonFunctions:convertTableToString(pts.policy_table.module_config, 1))
+  end
+  -- Section app_policies verified for '0000001' app only
+  if not is_table_equal(ptu.policy_table.app_policies["0000001"], pts.policy_table.app_policies["0000001"]) then
+    self:FailTestCase("Diffs in 'app_policies' section\nExpected:\n" .. commonFunctions:convertTableToString(ptu.policy_table.app_policies["0000001"], 1) .. "\nActual:\n" .. commonFunctions:convertTableToString(pts.policy_table.app_policies["0000001"], 1))
+  end
 end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
 
-function Test.Postcondition_RemovePTUfiles()
-  os.remove(ptu_app_registered)
-end
-
-function Test:Postcondition_Force_Stop_SDL()
-  commonFunctions:SDLForceStop(self)
+function Test.StopSDL()
+  StopSDL()
 end
 
 return Test
