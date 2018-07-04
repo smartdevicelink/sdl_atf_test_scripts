870e260d050cd8ec972ad7775639c25ace3aff52
HSavynetska <hsavynetska@luxoft.com>
2018-07-04 13:35:28 +0300

    Update script

--- test_scripts/Defects/5_0/1398_RegisterAppInterface_SDL_returns_DISALLOWED_instead_of_DUPLICATE_NAME.lua
+++ test_scripts/Defects/5_0/1398_RegisterAppInterface_SDL_returns_DISALLOWED_instead_of_DUPLICATE_NAME.lua
@@ -12,7 +12,7 @@
 -- Expected:
 -- 1) App is not registered, SDL returns DUPLICATE_NAME response
 -- Actual result:
--- 1) App is not registered, SDL returns DISALLOWED response
+-- 1) App is not registered, SDL returns APPLICATION_REGISTERED_ALREADY response
 ---------------------------------------------------------------------------------------------------
 --[[ Required Shared libraries ]]
 local runner = require('user_modules/script_runner')
@@ -21,45 +21,43 @@ local mobile_session = require("mobile_session")
 
 --[[ Local Functions ]]
 local function firstApplicationRegistered(self)
-  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
-  self.mobileSession:StartService(7)
+  self.mobileSession1 = mobile_session.MobileSession(self, self.mobileConnection)
+  self.mobileSession1:StartService(7)
   :Do(function()
-  local CorIdRegister = self.mobileSession:SendRPC("RegisterAppInterface",
-  {
-    syncMsgVersion = {
-      majorVersion = 3,
+    local CorIdRegister = self.mobileSession1:SendRPC("RegisterAppInterface",
+    {
+      syncMsgVersion = {
+        majorVersion = 3,
         minorVersion = 0
-    },
-  appName = "SyncProxyTester",
-  isMediaApplication = true,
-  languageDesired = 'EN-US',
-  hmiDisplayLanguageDesired = 'EN-US',
-    appID = "1"
-  })
-  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = "SyncProxyTester"} })
-    self.mobileSession:ExpectResponse(CorIdRegister, { success = true, resultCode = "SUCCESS" })
-    :Timeout(2000)
-    self.mobileSession:ExpectNotification("OnHMIStatus",{hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"})
+      },
+      appName = "SyncProxyTester",
+      isMediaApplication = true,
+      languageDesired = 'EN-US',
+      hmiDisplayLanguageDesired = 'EN-US',
+      appID = "1"
+    })
+    EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = "SyncProxyTester"} })
+      self.mobileSession1:ExpectResponse(CorIdRegister, { success = true, resultCode = "SUCCESS" })
+      self.mobileSession1:ExpectNotification("OnHMIStatus",{hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"})
   end)
 end
 
 local function RAI_DuplicateWithApp1AppNameId(self)
-  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
-  self.mobileSession:StartService(7)
+  self.mobileSession2 = mobile_session.MobileSession(self, self.mobileConnection)
+  self.mobileSession2:StartService(7)
   :Do(function()
-  local CorIdRegister = self.mobileSession:SendRPC("RegisterAppInterface",
-  {
-    syncMsgVersion = {
-    majorVersion = 3,
-    minorVersion = 0 },
-  appName = "SyncProxyTester",
-  isMediaApplication = true,
-  languageDesired = 'EN-US',
-  hmiDisplayLanguageDesired = 'EN-US',
-    appID = "1"
-  })
-    self.mobileSession:ExpectResponse(CorIdRegister, { success = false, resultCode = "DUPLICATE_NAME" })
-    :Timeout(2000)
+    local CorIdRegister = self.mobileSession2:SendRPC("RegisterAppInterface",
+    {
+      syncMsgVersion = {
+      majorVersion = 3,
+      minorVersion = 0 },
+      appName = "SyncProxyTester",
+      isMediaApplication = false,
+      languageDesired = 'EN-US',
+      hmiDisplayLanguageDesired = 'EN-US',
+      appID = "1"
+    })
+    self.mobileSession2:ExpectResponse(CorIdRegister, { success = false, resultCode = "DUPLICATE_NAME" })
   end)
 end
 
