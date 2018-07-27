bdbccb575459251eec92039231fddbba57243820
HSavynetska <hsavynetska@luxoft.com>
2018-07-27 10:25:01 +0300

    Update script by comments

--- test_scripts/Defects/5_0/1908_Non_media_app_must_be_activated_during_active_audio_source.lua
+++ test_scripts/Defects/5_0/1908_Non_media_app_must_be_activated_during_active_audio_source.lua
@@ -17,7 +17,18 @@
 local runner = require('user_modules/script_runner')
 local common = require('test_scripts/Defects/commonDefects')
 
+--[[ Test Configuration ]]
+config.application1.registerAppInterfaceParams.isMediaApplication = false
+config.application1.registerAppInterfaceParams.appHMIType = { "DEFAULT" }
+
 --[[ Local Functions ]]
+local function nonMediaActivateApp(self)
+  local requestId = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = common.getHMIAppId() })
+  EXPECT_HMIRESPONSE(requestId)
+  self.mobileSession1:ExpectNotification("OnHMIStatus",
+  { hmiLevel = "FULL", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN" })
+end
+
 local function onEventChange(self)
 	self.hmiConnection:SendNotification("BasicCommunication.OnEventChanged", {eventName = "AUDIO_SOURCE", isActive = true})
   self.mobileSession1:ExpectNotification("OnHMIStatus",
@@ -26,8 +37,8 @@ local function onEventChange(self)
   :Times(2)
   :Do(function(exp)
     if exp.occurences == 1 then
-      local requestId = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = common.getHMIAppId(1) })
-       EXPECT_HMIRESPONSE(requestId)
+      local requestId = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = common.getHMIAppId() })
+      EXPECT_HMIRESPONSE(requestId)
       self.hmiConnection:SendNotification("BasicCommunication.OnEventChanged", {eventName = "AUDIO_SOURCE", isActive = false})
     end
   end)
@@ -38,10 +49,10 @@ runner.Title("Preconditions")
 runner.Step("Clean environment", common.preconditions)
 runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
 runner.Step("RAI", common.rai_n)
-runner.Step("Activate App", common.activate_app)
+runner.Step("Non-media app activated", nonMediaActivateApp)
 
 runner.Title("Test")
-runner.Step("onEventChange EMBEDDED_NAVI true", onEventChange)
+runner.Step("Non_media app in BACKGROUND and NOT_AUDIBLE due to active embedded audio source", onEventChange)
 
 runner.Title("Postconditions")
 runner.Step("Stop SDL", common.postconditions)
