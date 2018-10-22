aef72268b11ecfb1cf5e42adeb14b88c9b6318c0
Myza Taras <tmyza@tmyza.kiev.luxoft.com>
2018-10-22 12:29:06 +0300

    Added changes according to comments for PR

--- test_scripts/Defects/5_0/2458_Need_change_OnButtonEventNotification_logic_for_OK_button.lua
+++ test_scripts/Defects/5_0/2458_Need_change_OnButtonEventNotification_logic_for_OK_button.lua
@@ -2,13 +2,17 @@
 -- User story: https://github.com/smartdevicelink/sdl_core/issues/2458
 --
 -- Description:
--- Need change OnButtonEventNotification logic for OK button
--- Precondition:
--- Core and HMI are started.
+-- 1) Need change OnButtonEventNotification logic for OK button
 -- In case:
--- 1) Activate first App with HMI Level - "FULL" and second App with HMI Level "LIMITED".
--- 2) Send On Button Event without App id, ExpectNotification only on App with "FULL" HMI level.
--- 3) Send On Button Event with App id, ExpectNotification on App with "FULL" and "LIMITED" HMI levels. 
+-- 1) First mobile app is subscribed on "OK" button and with "FULL" HMI Level
+-- 2) Second mobile app is subscribed on "OK" button and with "LIMITED" HMI Level
+-- SDL does:
+-- 1) respond SubscribeButton(SUCCESS) to mobile app1 and app2
+-- 2) send OnHashChange with updated hashId to mobile app1 and app2 after adding subscription
+-- 3) send OnButtonEventNotification to mobile app1 and app2
+-- Expected: 
+-- 1) Notification with AppID - HMI send notification to App with "FULL" and "LIMITED" HMI Levels
+-- 2) Notification without AppID - HMI send notification only App with "FULL" HMI Level.
 -- Actual result:
 -- 1) HMI send notification only App with "FULL" HMI Level.
 ---------------------------------------------------------------------------------------------------
@@ -18,6 +22,10 @@ local runner = require('user_modules/script_runner')
 
 --[[ Test Configuration ]]
 runner.testSettings.isSelfIncluded = false
+config.application1.registerAppInterfaceParams.appHMIType = { "MEDIA" }
+config.application1.registerAppInterfaceParams.isMediaApplication = true
+config.application2.registerAppInterfaceParams.appHMIType = { "NAVIGATION" }
+config.application2.registerAppInterfaceParams.isMediaApplication = false
 
 --[[ Local Functions ]]
 local function hmiLeveltoLimited(pAppId)
@@ -28,52 +36,88 @@ local function hmiLeveltoLimited(pAppId)
 end
 
 local function subscribeButton(pAppId)
-    local cid = common.getMobileSession(pAppId):SendRPC("SubscribeButton", { buttonName = "OK"})
-    common.getHMIConnection(pAppId):ExpectNotification("Buttons.OnButtonSubscription",
-        { appID = common.getHMIAppId(pAppId), name = "OK", isSubscribed = true })
-    common.getMobileSession(pAppId):ExpectResponse( cid, {success = true, resultCode = "SUCCESS"})
+    local cid = common.getMobileSession(pAppId):SendRPC("SubscribeButton", { buttonName = "OK" })
+    EXPECT_HMICALL("Buttons.SubscribeButton",{ appID = common.getHMIAppId(pAppId), buttonName = "OK" })
+      :Do(function(_, data)
+          common.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", { })
+        end)
+    common.getMobileSession(pAppId):ExpectResponse(cid, { success = true, resultCode = "SUCCESS" })
     common.getMobileSession(pAppId):ExpectNotification("OnHashChange")
-end
+  end
 
 local function OnButtonEventWithoutAppID()
-    common.getHMIConnection():SendNotification("Buttons.OnButtonEvent",
-    { name = "OK", mode = "BUTTONUP", appID = common.getHMIAppId(pAppId) })
-    common.getMobileSession(1):ExpectNotification( "OnButtonEvent")
-    :Times(0)
+    common.getHMIConnection(1):SendNotification("Buttons.OnButtonEvent",
+        { name = "OK", mode = "BUTTONDOWN" })
+    common.getHMIConnection(1):SendNotification("Buttons.OnButtonPress",
+        { name = "OK", mode = "SHORT" })
+    common.getHMIConnection(1):SendNotification("Buttons.OnButtonEvent",
+        { name = "OK", mode = "BUTTONUP" })
+
+    common.getMobileSession(1):ExpectNotification( "OnButtonEvent",
+        { buttonName = pButtonName, buttonEventMode = "BUTTONDOWN"},
+        { buttonName = pButtonName, buttonEventMode = "BUTTONUP"})
+        :Times(2)
+     common.getMobileSession(1):ExpectNotification( "OnButtonPress",
+        { buttonName = pButtonName, buttonPressMode = "SHORT"})    
+
     common.getMobileSession(2):ExpectNotification( "OnButtonEvent",
-        { buttonName = "OK", buttonEventMode = "BUTTONUP"})
+        { buttonName = pButtonName, buttonEventMode = "BUTTONDOWN"},
+        { buttonName = pButtonName, buttonEventMode = "BUTTONUP"})
+        :Times(0)
+    common.getMobileSession(2):ExpectNotification( "OnButtonPress",
+        { buttonName = pButtonName, buttonPressMode = "SHORT"})
+        :Times(0)    
 end
 
-local function OnButtonEventWithAppID(pParam1, pParam2)
-    common.getHMIConnection(pParam1):SendNotification("Buttons.OnButtonEvent",
-    { name = "OK", mode = "BUTTONUP", appID = common.getHMIAppId(pParam1) })
-    common.getHMIConnection(pParam2):SendNotification("Buttons.OnButtonEvent",
-    { name = "OK", mode = "BUTTONUP", appID = common.getHMIAppId(pParam2) })
-    common.getMobileSession(pParam1):ExpectNotification( "OnButtonEvent", 
-        { buttonName = "OK", buttonEventMode = "BUTTONUP"})
-    common.getMobileSession(pParam2):ExpectNotification( "OnButtonEvent", 
-        { buttonName = "OK", buttonEventMode = "BUTTONUP"})
-end
+local function OnButtonEventWithAppID()
+    common.getHMIConnection(1):SendNotification("Buttons.OnButtonEvent",
+        { name = "OK", mode = "BUTTONDOWN", appID = common.getHMIAppId(1) })
+    common.getHMIConnection(1):SendNotification("Buttons.OnButtonPress",
+        { name = "OK", mode = "SHORT", appID = common.getHMIAppId(1) })
+    common.getHMIConnection(1):SendNotification("Buttons.OnButtonEvent",
+        { name = "OK", mode = "BUTTONUP", appID = common.getHMIAppId(1) })
 
+    common.getHMIConnection(2):SendNotification("Buttons.OnButtonEvent",
+        { name = "OK", mode = "BUTTONDOWN", appID = common.getHMIAppId(2) })
+    common.getHMIConnection(2):SendNotification("Buttons.OnButtonPress",
+        { name = "OK", mode = "SHORT", appID = common.getHMIAppId(2) })
+    common.getHMIConnection(2):SendNotification("Buttons.OnButtonEvent",
+        { name = "OK", mode = "BUTTONUP", appID = common.getHMIAppId(2) })
+
+    common.getMobileSession(1):ExpectNotification( "OnButtonEvent",
+        { buttonName = pButtonName, buttonEventMode = "BUTTONDOWN"},
+        { buttonName = pButtonName, buttonEventMode = "BUTTONUP"})
+        :Times(2)
+     common.getMobileSession(1):ExpectNotification( "OnButtonPress",
+        { buttonName = pButtonName, buttonPressMode = "SHORT"})    
+
+    common.getMobileSession(2):ExpectNotification( "OnButtonEvent",
+        { buttonName = pButtonName, buttonEventMode = "BUTTONDOWN"},
+        { buttonName = pButtonName, buttonEventMode = "BUTTONUP"})
+        :Times(2)
+    common.getMobileSession(2):ExpectNotification( "OnButtonPress",
+        { buttonName = pButtonName, buttonPressMode = "SHORT"})    
+end
 
 --[[ Scenario ]]
 runner.Title("Preconditions")
 runner.Step("Clean environment", common.preconditions)
 runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
-
 runner.Step("Register App1", common.registerAppWOPTU, { 1 })
 runner.Step("Activate App1", common.activateApp, { 1 })
-runner.Step("Hmi level LIMITED", hmiLeveltoLimited, { 1 })
-runner.Step("SubscribeButton App1", subscribeButton, { 1 })
-
 runner.Step("Register App2", common.registerAppWOPTU, { 2 })
-runner.Step("Activate App2(HMI Level FULL)", common.activateApp, { 2 })
-runner.Step("SubscribeButton App2", subscribeButton, { 2 })
+runner.Step("Activate App2", common.activateApp, { 2 })
+
+runner.Step("Subscribe on button OK App1", subscribeButton, { 1 })
+runner.Step("Subscribe on button OK App2", subscribeButton, { 2 })
+
+runner.Step("Set App1 HMI Level to Limited)", hmiLeveltoLimited, { 2 })
+runner.Step("Activate App1", common.activateApp, { 1 })
 
 -- [[ Test ]]
 runner.Title("Test")
+runner.Step("OnButtonEvent with App ID", OnButtonEventWithAppID)
 runner.Step("OnButtonEvent without App ID", OnButtonEventWithoutAppID)
-runner.Step("OnButtonEvent with App ID", OnButtonEventWithAppID, { 1, 2 })
 
 -- [[ Postconditions ]]
 runner.Title("Postconditions")
