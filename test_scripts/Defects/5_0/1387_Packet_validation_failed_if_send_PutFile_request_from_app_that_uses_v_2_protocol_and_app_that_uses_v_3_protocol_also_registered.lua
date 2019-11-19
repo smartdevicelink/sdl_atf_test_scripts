5265af60d36d48820cf940c0bcda6d6f7d249849
Galina <hsavynetska@luxoft.com>
2019-11-19 17:46:40 +0200

    Updating the test script and rebase to the current development branch

--- test_scripts/Defects/5_0/1387_Packet_validation_failed_if_send_PutFile_request_from_app_that_uses_v_2_protocol_and_app_that_uses_v_3_protocol_also_registered.lua
+++ test_scripts/Defects/5_0/1387_Packet_validation_failed_if_send_PutFile_request_from_app_that_uses_v_2_protocol_and_app_that_uses_v_3_protocol_also_registered.lua
@@ -3,16 +3,17 @@
 --
 -- Description:
 --
--- In case:
+-- Precondition:
 -- 1) "MaximumPayloadSize" = 1488 in smartDeviceLink.ini file
 -- 2) SDL and HMI are started
--- 3) app1 is registered over v.2 protocol, app2 is registered over v.3 protocol
--- 4) app1 is activated
+-- 3) App1 is registered over v.2 protocol, app2 is registered over v.3 protocol
+-- 4) App1 is activated
+-- Step:
 -- 1) Send PutFile from app1 png_20kb.png.
--- Expected result:
--- 1) Request processed successfully
---    SDL respond with success resultcode
---    Note: There is no defect if only app1 (over v.2 protocol) registered
+-- SDL does:
+-- 1) request processed successfully
+-- 2) respond with success resultCode
+-- Note: There is no defect if only app1 (over v.2 protocol) registered
 ---------------------------------------------------------------------------------------------------
 --[[ Required Shared libraries ]]
 local common = require('user_modules/sequences/actions')
@@ -29,53 +30,48 @@ runner.testSettings.isSelfIncluded = false
 local pMaxPayloadSize = 1488
 
 local putFileParams = {
-    requestParams = {
-        syncFileName = 'png_20kb.png',
-        fileType = "GRAPHIC_PNG"
-    },
-    filePath = "files/png_20kb.png"
+  requestParams = {
+    syncFileName = 'png_20kb.png',
+    fileType = "GRAPHIC_PNG"
+  },
+  filePath = "files/png_20kb.png"
 }
 
 --[[ Local Functions ]]
-local function backupINIFile()
-    commonPreconditions:BackupFile("smartDeviceLink.ini")
-end
-
 local function updateINIFile()
-    backupINIFile()
-    commonFunctions:write_parameter_to_smart_device_link_ini("MaximumPayloadSize", pMaxPayloadSize)
+  commonPreconditions:BackupFile("smartDeviceLink.ini")
+  commonFunctions:write_parameter_to_smart_device_link_ini("MaximumPayloadSize", pMaxPayloadSize)
 end
 
 local function createdSession1()
-    config.defaultProtocolVersion = 2
-    test.mobileSession[1] = mobile_session.MobileSession(
-    test,
-    test.mobileConnection,
-    config.application1.registerAppInterfaceParams)
-    common.getMobileSession(1).version = 2
-    common.getMobileSession(1).answerHeartbeatFromSDL = true
-    common.getMobileSession(1).sendHeartbeatToSDL = true
+  config.defaultProtocolVersion = 2
+  test.mobileSession[1] = mobile_session.MobileSession(
+  test,
+  test.mobileConnection,
+  config.application1.registerAppInterfaceParams)
+  common.getMobileSession(1).version = 2
+  common.getMobileSession(1).answerHeartbeatFromSDL = true
+  common.getMobileSession(1).sendHeartbeatToSDL = true
 end
 
 local function createdSession2()
-    config.defaultProtocolVersion = 3
-    test.mobileSession[2] = mobile_session.MobileSession(
-    test,
-    test.mobileConnection,
-    config.application2.registerAppInterfaceParams)
-    common.getMobileSession(2).version = 3
-    common.getMobileSession(2).answerHeartbeatFromSDL = true
-    common.getMobileSession(2).sendHeartbeatToSDL = true
+  config.defaultProtocolVersion = 3
+  test.mobileSession[2] = mobile_session.MobileSession(
+  test,
+  test.mobileConnection,
+  config.application2.registerAppInterfaceParams)
+  common.getMobileSession(2).version = 3
+  common.getMobileSession(2).answerHeartbeatFromSDL = true
+  common.getMobileSession(2).sendHeartbeatToSDL = true
 end
 
-local function putFile(params, pAppId)
-    if not pAppId then pAppId = 1 end
-    local cid = common.getMobileSession():SendRPC("PutFile", params.requestParams, params.filePath)
-    common.getMobileSession():ExpectResponse(cid, { success = true, resultCode = "SUCCESS"})
+local function putFile(params)
+  local cid = common.getMobileSession():SendRPC("PutFile", params.requestParams, params.filePath)
+  common.getMobileSession():ExpectResponse(cid, { success = true, resultCode = "SUCCESS"})
 end
 
 local function restoreINIFile()
-    commonPreconditions:RestoreFile("smartDeviceLink.ini")
+  commonPreconditions:RestoreFile("smartDeviceLink.ini")
 end
 
 --[[ Scenario ]]
@@ -83,14 +79,15 @@ runner.Title("Preconditions")
 runner.Step("Clean environment", common.preconditions)
 runner.Step("Update INI file", updateINIFile)
 runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
-runner.Step("Created mobile session for app1", createdSession1)
+runner.Step("Mobile session for app1", createdSession1)
 runner.Step("Register App1", common.registerApp, { 1 })
-runner.Step("Created mobile session for app2", createdSession2)
+runner.Step("Mobile session for app2", createdSession2)
 runner.Step("Register App2", common.registerAppWOPTU, { 2 })
 runner.Step("Activate App1", common.activateApp)
 
 -- [[ Test ]]
-runner.Step("Upload icon file", putFile, { putFileParams })
+runner.Title("Test")
+runner.Step("Upload Icon File from the App1", putFile, { putFileParams })
 
 -- [[ Postconditions ]]
 runner.Title("Postconditions")
