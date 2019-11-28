6e1d9ce7e148ebbd1670ee719cf99d73dc4ecd9c
Myza Taras <tmyza@tmyza.kiev.luxoft.com>
2019-11-28 17:33:24 +0300

    Fix for script

--- test_scripts/Defects/5_0/2442_Fix_mem_seg_fault_error_in_jsoncpp.lua
+++ test_scripts/Defects/5_0/2442_Fix_mem_seg_fault_error_in_jsoncpp.lua
@@ -2,71 +2,73 @@
 -- User story: https://github.com/smartdevicelink/sdl_core/issues/2442
 --
 -- Description:
--- The root cause of the issue is a stack overflow which is occurring due to recursive call of deeply nested areas 
--- and objects. We have readValue(..) which calls readObject(..) or readArray(..), which call readValue(...). 
+-- The root cause of the issue is a stack overflow which is occurring due to recursive call of deeply nested areas
+-- and objects. We have readValue(..) which calls readObject(..) or readArray(..), which call readValue(...).
 -- Steps to reproduce:
--- 1) Generate Json file with more than 50 nestings.
--- 2) The starting point is a call to parse(..).
--- Expected:
--- 1) When recursion threshold is exceeded (50 nestings) SDL should STOP.
+-- 1) SDL and HMI are started
+-- 2) App1 is registered and triggered PTU
+-- 3) Json file with 50 nestings for PTU, PTU is performed successful
+-- 4) App2 is registered and triggered PTU
+-- 5) Json file with 51 nestings for PTU
+-- SDL does:
+-- - a) Ignore json file
+-- - b) PTU is performed unsuccessful
 ---------------------------------------------------------------------------------------------------
 -- [[ Required Shared libraries ]]
 local runner = require('user_modules/script_runner')
-local commonRC = require('test_scripts/RC/commonRC')
-local hmi_values = require('user_modules/hmi_values')
-local test = require('user_modules/dummy_connecttest')
-local utils = require("user_modules/utils")
-local SDL = require('SDL')
+local common = require('user_modules/sequences/actions')
 
  -- [[ Test Configuration ]]
 runner.testSettings.isSelfIncluded = false
 
---  [[ Local Functions ]]
-local function getRTable(deep)
-    if nil == deep then deep  = 0 end
-    if 0 == deep then
-        return {};
-    end
-    local curentrez = {};
-    curentrez[#curentrez .. tostring(deep)] = getRTable(deep - 1);
-    return curentrez;    
+-- --  [[ Local Functions ]]
+local function pTUpdateFunc1(tbl)
+  local Group = {
+    rpcs = {
+      Data = {
+        hmi_levels = {
+          {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l =
+          {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l =
+          {l50nestings = {"FULL"}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+        }
+      }
+    }
+  }
+  tbl.policy_table.functional_groupings["Group"] = Group
+  tbl.policy_table.app_policies[config.application1.registerAppInterfaceParams.fullAppID].groups = {"Base-4", "Group"}
 end
 
-local function getHMIParams()
-    local params = hmi_values.getDefaultHMITable()
-    JsonTable = getRTable(51)
-    params.RC.GetCapabilities.params = JsonTable
-    return params
+local function pTUpdateFunc2(tbl)
+  local Group = {
+    rpcs = {
+      Data2 = {
+        hmi_levels = {
+          {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l =
+          {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l =
+          {l = {l51nestings = {"FULL"}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+        }
+      }
+    }
+  }
+  tbl.policy_table.functional_groupings["Group"] = Group
+  tbl.policy_table.app_policies[config.application2.registerAppInterfaceParams.fullAppID].groups = {"Base-4", "Group"}
 end
 
-local function funcCausesStopOfSDL()
-    commonRC.start(getHMIParams())    
-    utils.wait(10000)   
+local function expNotificationFunc()
+  common.getHMIConnection():ExpectNotification("SDL.OnStatusUpdate",
+    { status = "UPDATING" }, { status = "UPDATE_NEEDED" })
+  :Times(2)
 end
 
-function failTestCase(pCause)
-    test:FailTestCase(pCause)
-end
+runner.Title("Preconditions")
+runner.Step("Clean environment", common.preconditions)
+runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+runner.Step("Register App1", common.registerApp, { 1 })
+runner.Step("PTU, json with 50 nestings", common.policyTableUpdate, { pTUpdateFunc1 })
 
-local  function checkStatusSDL()    
-    if SDL:CheckStatusSDL() == SDL.STOP  then        
-        return true, "SDL was stoped"
-    end
-    if SDL:CheckStatusSDL() == SDL.RUNNING then
-        failTestCase("SDL was not stopped")
-    end
-end
-
- --[[ Scenario ]]
- runner.Title("Preconditions")
- runner.Step("Backup HMI capabilities file", commonRC.backupHMICapabilities)
- runner.Step("Clean environment", commonRC.preconditions)
-
- -- [[ Test ]]
 runner.Title("Test")
-runner.Step("Start SDL, HMI, connect Mobile, start Session", funcCausesStopOfSDL)
-runner.Step("Check status SDL", checkStatusSDL)
+runner.Step("Register App2", common.registerApp, { 2 })
+runner.Step("PTU, json with 51 nestings", common.policyTableUpdate, { pTUpdateFunc2, expNotificationFunc })
 
- -- [[ Postconditions ]]
 runner.Title("Postconditions")
-runner.Step("Stop SDL", commonRC.postconditions) 
+runner.Step("Stop SDL", common.postconditions)
