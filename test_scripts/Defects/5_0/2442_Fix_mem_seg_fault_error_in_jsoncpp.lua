95d0fc3565964cd76423f4be82c027c82c11420a
Myza Taras <tmyza@tmyza.kiev.luxoft.com>
2020-01-23 13:50:22 +0300

    Fix for script according to changes in SDL core

--- test_scripts/Defects/5_0/2442_Fix_mem_seg_fault_error_in_jsoncpp.lua
+++ test_scripts/Defects/5_0/2442_Fix_mem_seg_fault_error_in_jsoncpp.lua
@@ -7,9 +7,9 @@
 -- Steps to reproduce:
 -- 1) SDL and HMI are started
 -- 2) App1 is registered and triggered PTU
--- 3) Json file with 50 nestings for PTU, PTU is performed successful
+-- 3) Json file with 995 nestings for PTU, PTU is performed successful
 -- 4) App2 is registered and triggered PTU
--- 5) Json file with 51 nestings for PTU
+-- 5) Json file with 996 nestings for PTU
 -- SDL does:
 -- - a) Ignore json file
 -- - b) PTU is performed unsuccessful
@@ -22,17 +22,18 @@ local common = require('user_modules/sequences/actions')
 runner.testSettings.isSelfIncluded = false
 
 -- --  [[ Local Functions ]]
+local function getRTable(deep)
+  if 0 == deep then
+      return {}
+  end
+  local curentrez = {}
+  curentrez[#curentrez .. tostring(deep)] = getRTable(deep - 1)
+  return curentrez
+end
+
 local function pTUpdateFunc1(tbl)
   local Group = {
-    rpcs = {
-      Data = {
-        hmi_levels = {
-          {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l =
-          {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l =
-          {l50nestings = {"FULL"}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-        }
-      }
-    }
+    rpcs = getRTable(995)
   }
   tbl.policy_table.functional_groupings["Group"] = Group
   tbl.policy_table.app_policies[config.application1.registerAppInterfaceParams.fullAppID].groups = {"Base-4", "Group"}
@@ -40,35 +41,25 @@ end
 
 local function pTUpdateFunc2(tbl)
   local Group = {
-    rpcs = {
-      Data2 = {
-        hmi_levels = {
-          {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l =
-          {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l = {l =
-          {l = {l51nestings = {"FULL"}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-        }
-      }
-    }
+    rpcs = getRTable(996)
   }
   tbl.policy_table.functional_groupings["Group"] = Group
   tbl.policy_table.app_policies[config.application2.registerAppInterfaceParams.fullAppID].groups = {"Base-4", "Group"}
 end
 
 local function expNotificationFunc()
-  common.getHMIConnection():ExpectNotification("SDL.OnStatusUpdate",
-    { status = "UPDATING" }, { status = "UPDATE_NEEDED" })
-  :Times(2)
+  common.getHMIConnection():ExpectNotification("SDL.OnStatusUpdate", { status = "UPDATE_NEEDED" })
 end
 
 runner.Title("Preconditions")
 runner.Step("Clean environment", common.preconditions)
 runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
 runner.Step("Register App1", common.registerApp, { 1 })
-runner.Step("PTU, json with 50 nestings", common.policyTableUpdate, { pTUpdateFunc1 })
+runner.Step("Success PTU, json with 995 nestings", common.policyTableUpdate, { pTUpdateFunc1 })
 
 runner.Title("Test")
 runner.Step("Register App2", common.registerApp, { 2 })
-runner.Step("PTU, json with 51 nestings", common.policyTableUpdate, { pTUpdateFunc2, expNotificationFunc })
+runner.Step("Unsuccess PTU, json with 996 nestings", common.policyTableUpdate, { pTUpdateFunc2, expNotificationFunc })
 
 runner.Title("Postconditions")
 runner.Step("Stop SDL", common.postconditions)
