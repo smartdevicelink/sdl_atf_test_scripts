8ba1b50ca0cd78f6e8e9aaa5f12b756ca08374b3
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2020-06-30 13:20:51 -0400

    Remove debug code

--- test_scripts/Policies/build_options/079_ATF_PTU_HMI_Level_Affected_Apps_NONE_BACKGROUND_HTTP.lua
+++ test_scripts/Policies/build_options/079_ATF_PTU_HMI_Level_Affected_Apps_NONE_BACKGROUND_HTTP.lua
@@ -172,7 +172,6 @@ function Test.Precondition_Clean()
   if not check_file_exists(policy_file_path .. "/sdl_snapshot.json") then
     print("PTS is removed")
   end
-  os.execute("ls -la " .. config.pathToSDL .. "storage/")
 end
 
 function Test.Precondition_StartSDL()
