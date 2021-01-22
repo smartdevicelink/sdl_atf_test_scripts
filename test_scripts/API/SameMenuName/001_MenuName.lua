4515ef18fa31f8d55103d177a172e83b175a24ac
collin <collin+i@collinmcqueen.com>
2021-01-22 11:20:57 -0500

    test sub menu duplicate names

--- test_scripts/API/SameMenuName/001_MenuName.lua
+++ test_scripts/API/SameMenuName/001_MenuName.lua
@@ -1,6 +1,6 @@
 ---------------------------------------------------------------------------------------------------
 -- Proposal: SDL 0180 Broaden Choice Uniqueness
--- 
+--
 -- Description:
 --   Mobile shall be able to send two AddCommand RPCs with identical menuName values.
 ---------------------------------------------------------------------------------------------------
@@ -28,16 +28,42 @@ local requestParams2 = {
   menuParams = menuParams
 }
 
+local requestParams3 = {
+  menuID = 699,
+  menuName = "menuName"
+}
+
+local requestParams4 = {
+  menuID = 700,
+  menuName = "menuName"
+}
+
 --[[ Local Functions ]]
 local function addCommand(pParams)
   local cid = common.getMobileSession():SendRPC("AddCommand", pParams)
 
-  local hmiRequestParams = { 
+  local hmiRequestParams = {
     cmdID = pParams.cmdID,
     menuParams = pParams.menuParams,
     appID = common.getHMIAppId()
   }
   common.getHMIConnection():ExpectRequest("UI.AddCommand", hmiRequestParams)
+  :Do(function(_, data)
+    common.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", {})
+  end)
+
+  common.getMobileSession():ExpectResponse(cid, { success = true, resultCode = "SUCCESS" })
+  common.getMobileSession():ExpectNotification("OnHashChange")
+end
+
+local function addSubMenu(pParams)
+  local cid = common.getMobileSession():SendRPC("AddSubMenu", pParams)
+
+  local hmiRequestParams = {
+    menuID = pParams.menuID,
+    appID = common.getHMIAppId()
+  }
+  common.getHMIConnection():ExpectRequest("UI.AddSubMenu", hmiRequestParams)
   :Do(function(_, data)
     common.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", {})
   end)
@@ -57,6 +83,8 @@ runner.Step("Activate App", common.activateApp)
 runner.Title("Test")
 runner.Step("AddCommand Positive Case", addCommand, { requestParams1 })
 runner.Step("AddCommand With Same Menu Name", addCommand, { requestParams2 })
+runner.Step("AddSubMenu With Same Menu Name", addSubMenu, { requestParams3 })
+runner.Step("AddSubMenu Again With Same Menu Name", addSubMenu, { requestParams4 })
 
 runner.Title("Postconditions")
 runner.Step("Stop SDL", common.postconditions)
