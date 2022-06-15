c13adbcc55fce80c532a4c9713fc83888de4c819
GetmanetsIrina <igetmanets@luxoft.com>
2022-06-15 15:11:29 +0300

    Moved scripts to 8_2 scripts

--- test_scripts/Defects/8_2/3858/3858_2_CreateInteractionChoiceSet_custom_success_codes_error_structure.lua
+++ test_scripts/Defects/8_2/3858/3858_2_CreateInteractionChoiceSet_custom_success_codes_error_structure.lua
@@ -1,7 +1,7 @@
 ---------------------------------------------------------------------------------------------------
 -- Issue: https://github.com/smartdevicelink/sdl_core/issues/3858
 ---------------------------------------------------------------------------------------------------
--- Description: Check that SDL transfers success result code in result structure from HMI to the mobile app
+-- Description: Check that SDL transfers success result code in error structure from HMI to the mobile app
 -- during processing of CreateInteractionChoiceSet
 
 -- Precondition:
@@ -11,14 +11,14 @@
 -- Steps:
 -- 1. Mobile app requests CreateInteractionChoiceSet RPC with vrCommands
 -- 2. SDL sends VR.AddCommand(type="Choice") request to the HMI
--- 3. HMI responds with success result code in result structure to VR.AddCommand
+-- 3. HMI responds with success result code in error structure to VR.AddCommand
 --
 -- SDL does:
 -- 1. send CreateInteractionChoiceSet(success = true, resultCode = <code received from HMI>) response to the mobile app
 ---------------------------------------------------------------------------------------------------
 --[[ Required Shared libraries ]]
 local runner = require('user_modules/script_runner')
-local common = require('test_scripts/Defects/8_1/3858/common_3858')
+local common = require('test_scripts/Defects/8_2/3858/common_3858')
 
 --[[ Test Configuration ]]
 runner.testSettings.isSelfIncluded = false
@@ -32,7 +32,7 @@ runner.Step("Activate App", common.activateApp)
 
 runner.Title("Test")
 for choiceId, resultCode in ipairs(common.tcs) do
-  local response = { code = resultCode, structure = common.responsesStructures.result }
+  local response = { code = resultCode, structure = common.responsesStructures.error }
   runner.Title("Test case: '" .. tostring(resultCode) .. "'" )
   runner.Step("CreateInteractionChoiceSet response with success = true", common.createInteractionChoiceSet,
     { choiceId, response })
