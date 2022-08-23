bda5f73db4a72d207a28e2d3d5022633cb175d91
Shobhit Adlakha <ShobhitAd@users.noreply.github.com>
2022-08-23 16:26:14 -0400

    Fix/Increase timeout for failed resumption test (#2699)
    * Increase timeout for reRegisterAppResumeFailed step
    
    * Increase timeout for similar late response script
    
    * Address review comments
    
    * Increase timeout for 020

--- test_scripts/Resumption/Handling_errors_from_HMI/014_resume_one_rpc_late_response_ign_off.lua
+++ test_scripts/Resumption/Handling_errors_from_HMI/014_resume_one_rpc_late_response_ign_off.lua
@@ -54,7 +54,7 @@ for k, value in common.pairs(common.rpcs) do
     runner.Step("Unexpected disconnect", common.unexpectedDisconnect)
     runner.Step("Connect mobile", common.connectMobile)
     runner.Step("Reregister App resumption " .. k, common.reRegisterAppResumeFailed,
-      { 1, common.checkResumptionData, common.resumptionFullHMILevel, k, interface, 15000})
+      { 1, common.checkResumptionData, common.resumptionFullHMILevel, k, interface, 20000})
     runner.Step("Unregister App", common.unregisterAppInterface)
   end
 end
--- test_scripts/Resumption/Handling_errors_from_HMI/055_resume_IVD_subscription_without_response_all_moduleTypes.lua
+++ test_scripts/Resumption/Handling_errors_from_HMI/055_resume_IVD_subscription_without_response_all_moduleTypes.lua
@@ -84,7 +84,7 @@ end
 runner.Step("Unexpected disconnect", common.unexpectedDisconnect, { #common.rcModuleTypes })
 runner.Step("Connect mobile", common.connectMobile)
 runner.Step("Reregister App resumption data", common.reRegisterAppResumeFailed,
-  { 1, checkResumptionData, common.resumptionFullHMILevel, nil, nil, 15000 })
+  { 1, checkResumptionData, common.resumptionFullHMILevel, nil, nil, 20000 })
 for _, moduleType in pairs(common.rcModuleTypes) do
   runner.Step("Check no subscriptions for getInteriorVehicleData ".. moduleType, common.isSubscribed,
     { false, nil, moduleType })
